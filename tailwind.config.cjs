143a5fd92bd52416162639f22885a7b125b383da
violeine <haluudang@gmail.com>
2022-09-19 12:41:32 +0700

    oops

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,astro}"],
 
   theme: {
     extend: {
