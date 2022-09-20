c22cd2d4f7d85d15ddfc492c53eb609b05812694
violeine <haluudang@gmail.com>
2022-09-20 20:30:08 +0700

    astro the right way hehe

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,36 @@
 
 const defaultTheme = require("tailwindcss/defaultTheme");
 
+// shamelessly took from https://github.dev/thien-do/memos.pub/blob/main/lib/blog/page/main.tsx :awkward:
+
+// For the typography plugin, we prefer Element Modifiers over direct CSS
+// configuration here. Should only use this for stuff that cannot be applied
+// via Element Modifiers.
+// - Also note that CSS config here has less specificity than Element Modifiers
+// (this uses :where() while modifier uses :is())
+// - https://tailwindcss.com/docs/typography-plugin#element-modifiers
+// - https://tailwindcss.com/docs/typography-plugin#customizing-the-css
+const typography = (_theme) => ({
+  DEFAULT: {
+    css: {
+      // Define here so that Tailwind's "pre code" can override them
+      code: { padding: "0.2em 0.4em", fontWeight: 400 },
+      "h1 code": { fontWeight: "inherit" },
+      "h2 code": { fontWeight: "inherit" },
+      "h3 code": { fontWeight: "inherit" },
+      // Matches both <pre> and <code>
+      '[data-theme="dark"]': { display: "none" },
+    },
+  },
+  invert: {
+    css: {
+      '[data-theme="light"]': { display: "none" },
+      // Reverse the "DEFAULT" style
+      'pre[data-theme="dark"]': { display: "block" },
+      'code[data-theme="dark"]': { display: "inline" },
+    },
+  },
+});
 module.exports = {
   content: ["./src/**/*.{html,js,astro}"],
 
@@ -13,7 +43,8 @@ module.exports = {
       maxWidth: {
         "prose-80": "80ch",
       },
+      typography: typography,
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
