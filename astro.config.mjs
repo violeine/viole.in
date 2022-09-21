bc108be5435089852a0144a3d911af77be34fbd4
violeine <haluudang@gmail.com>
2022-09-21 17:34:06 +0700

    add quine

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-
 import svelte from "@astrojs/svelte";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), react(), svelte()],
+  output: "server",
+  adapter: vercel()
 });
