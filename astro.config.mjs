938b6776a1515ea44850dbc0b99215b1c37fb330
violeine <haluudang@gmail.com>
2022-11-13 23:44:42 +0700

    :)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,12 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      theme: "github-light",
+      wrap: true,
+    },
+  },
   integrations: [
     tailwind({
       config: { applyBaseStyles: false },
