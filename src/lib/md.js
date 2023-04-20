55879e9911284ddd9446659950436d864169b21a
violeine <haluudang@gmail.com>
2023-04-21 00:27:30 +0700

    support inline md

--- src/lib/md.js
+++ src/lib/md.js
@@ -2,9 +2,15 @@ import { renderMarkdown } from "@astrojs/markdown-remark"
 
 export const md = Symbol.for("md");
 
-export const render = async (str) => {
+export const render = async (str, isInline) => {
   const t = await renderMarkdown(str, globalThis[md]);
-  return t.code;
+  return isInline ? inline(t.code) : t.code;
+}
+
+export const inline = str => {
+  //has <p> tag at start and end 
+  const hasP = str.indexOf('<p>') === 0 && str.indexOf('</p>') === str.length - 4;
+  return hasP ? str.slice(3, -4) : str;
 }
 export const mdIntegration = () => {
   return {
