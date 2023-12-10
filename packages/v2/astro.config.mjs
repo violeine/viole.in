e1a4d770d03e8f3155b6f826bd4c9ae5af60f7b4
violeine <haluudang@gmail.com>
2023-12-10 23:01:51 +0700

    feat: bare style

--- packages/v2/astro.config.mjs
+++ packages/v2/astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import { mdIntegration } from '@viole.in/md'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    syntaxHighlight: false
+  }, integrations: [mdIntegration()]
+});
